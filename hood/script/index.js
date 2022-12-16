61ee8cd07173dec7e6592db2ac3ec460bc92d1dc
impulses <impulses@gmail.com>
2022-12-16 10:30:26 +0530

    16Dec v0.1

--- hood/script/index.js
+++ hood/script/index.js
@@ -4,26 +4,30 @@
 
 /* When the window loads */
 window.onload = function() {
-    fnDims('xDims'); /* Show window dimensions */
-    fnSetTheme();
+  fnSetTheme();
+  fnDimensions('WxH'); 
 }
 /* - - - - - - - - */
 
-window.fnDims = function fnDims() {
-    document.getElementById('xDims').innerHTML = ( window.innerWidth+'x' + window.innerHeight );
-    window.addEventListener("resize", fnDims);
+/* fnDimensions – Show window size */
+window.fnDimensions = function fnDimensions(Ele) {
+  try {
+    (Ele) ? document.getElementById(`${Ele}`).innerHTML = ( window.innerWidth+'x' + window.innerHeight ) : null ;
+  } catch (err){};
+  window.addEventListener("resize", fnDimensions);
 }
 /* - - - - - - - - */
 
-/* fnActive | Looks for elements with the same className siblings, marks 'this' element as active. */
-window.fnActive = function fnActive(Element) {   // Workaround to use Module in HTML
+/* fnActive – Looks for elements with the same className siblings, marks 'this' element as active. */
+window.fnActive = function fnActive(Element) {
     let Class = Element.className.split(" ")[0]; // Get the first className
     Class ? document.querySelectorAll(`.${Class}`).forEach((Item) => { Item.classList.toggle('active', Element === Item) }) : null
 }
 /* - - - - - - - - */
 
-window.fnSetTheme = function fnSetTheme() { // Toggle Themes //
-
+/* fnSetTheme – Toggle Dark & Light Themes. */
+window.fnSetTheme = function fnSetTheme() {
+  /* window.fnX = function fnX() is a workaround to use module in HTML */
   const root = document.documentElement;
   const checkBox = document.querySelector('#Toggler');
   const xKey = 'DasaTheme';
@@ -49,6 +53,5 @@ window.fnSetTheme = function fnSetTheme() { // Toggle Themes //
 // var foo = 10;  
 // foo == 10 && doSomething(); // is the same thing as if (foo == 10) doSomething(); 
 // foo == 5 || doSomething(); // is the same thing as if (foo != 5) doSomething();
-
 // arg1 = arg1 || 10; // arg1 will have 10 as a default value if it’s not already set
 /* - - - - - - - - */
